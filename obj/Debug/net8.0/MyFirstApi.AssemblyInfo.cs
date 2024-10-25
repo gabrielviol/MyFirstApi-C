4a8acd22a9b7e4714e8adc0d4b50a15be7161e11
Gabriel Oliveira <gabrielviol00@hotmail.com>
2024-10-25 10:09:17 -0300

    [WIP]

--- obj/Debug/net8.0/MyFirstApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyFirstApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43524e8a10c9e03a71632b7b3f58e587043c1f60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da99216cd8ba32748bbd70a3de2b90a1f447c9c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
